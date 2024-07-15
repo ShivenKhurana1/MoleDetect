1f2a1748b6a477be989f4e03e7a77fae948cb173
Shiven Khurana <shivenbk@gmail.com>
2024-07-14 21:31:55 -0400

    Update home_view.dart

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,53 +1,218 @@
 import 'package:flutter/material.dart';
 import 'package:MoleDetect/constants/constants.dart';
 import 'package:MoleDetect/views/camera_view.dart';
+import 'package:MoleDetect/Animations/FadeAnimation.dart';
+import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
-class HomeView extends StatefulWidget {
-  const HomeView({super.key});
+void main() => runApp(
+  MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomePage()
+  )
+);
 
+class HomePage extends StatefulWidget {
   @override
-  State<HomeView> createState() => _HomeViewState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _HomeViewState extends State<HomeView> {
+class _HomePageState extends State<HomePage> with TickerProviderStateMixin{
+
+  AnimationController _scaleController;
+  AnimationController _scale2Controller;
+  AnimationController _widthController;
+  AnimationController _positionController;
+
+  Animation<double> _scaleAnimation;
+  Animation<double> _scale2Animation;
+  Animation<double> _widthAnimation;
+  Animation<double> _positionAnimation;
+
+  bool hideIcon = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _scaleController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300)
+    );
+
+    _scaleAnimation = Tween<double>(
+      begin: 1.0, end: 0.8
+    ).animate(_scaleController)..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _widthController.forward();
+      }
+    });
+
+    _widthController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 600)
+    );
+
+    _widthAnimation = Tween<double>(
+      begin: 80.0,
+      end: 300.0
+    ).animate(_widthController)..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _positionController.forward();
+      }
+    });
+
+    _positionController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 1000)
+    );
+
+    _positionAnimation = Tween<double>(
+      begin: 0.0,
+      end: 215.0
+    ).animate(_positionController)..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        setState(() {
+          hideIcon = true;
+        });
+        _scale2Controller.forward();
+      }
+    });
+
+    _scale2Controller = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300)
+    );
+
+    _scale2Animation = Tween<double>(
+      begin: 1.0,
+      end: 32.0
+    ).animate(_scale2Controller)..addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: camera_view()));
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    final double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Align(alignment: Alignment.topLeft, child: Images.hipaa),
-              Images.homeHero,
-              const SizedBox(height: 60),
-              Images.largeLogo,
-              const SizedBox(height: 20),
-              Text(
-                'AI-Powered Skin Cancer Detection for a Healthier Tomorrow',
-                style: TextStyles.smallHeadlineBlue,
-                textAlign: TextAlign.center,
-              ),
-              const Spacer(),
-              Row(
-                children: [
-                  Expanded(
-                    child: FilledButton(
-                        onPressed: () => Navigator.of(context).push(
-                              MaterialPageRoute(
-                                settings:
-                                    const RouteSettings(name: '/cameraView'),
-                                builder: (context) => const CameraView(),
-                              ),
+      backgroundColor: Color.fromRGBO(1, 8, 24, 1),
+      body: Container(
+        width: double.infinity,
+        child: Stack(
+          children: <Widget>[
+            Positioned(
+              top: -50,
+              left: 0,
+              child: FadeAnimation(1, Container(
+                width: width,
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/one.png'),
+                    fit: BoxFit.cover
+                  )
+                ),
+              )),
+            ),
+            Positioned(
+              top: -100,
+              left: 0,
+              child: FadeAnimation(1.3, Container(
+                width: width,
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/one.png'),
+                    fit: BoxFit.cover
+                  )
+                ),
+              )),
+            ),
+            Positioned(
+              top: -150,
+              left: 0,
+              child: FadeAnimation(1.6, Container(
+                width: width,
+                height: 400,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/images/one.png'),
+                    fit: BoxFit.cover
+                  )
+                ),
+              )),
+            ),
+            Container(
+              padding: EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  FadeAnimation(1, Text("MoleDetect", 
+                  style: TextStyle(color: Colors.white, fontSize: 50),)),
+                  SizedBox(height: 15,),
+                  FadeAnimation(1.3, Text("AI-Powered Skin Cancer Detection for a Healthier Tomorrow", 
+                  style: TextStyle(color: Colors.white.withOpacity(.7), height: 1.4, fontSize: 20),)),
+                  SizedBox(height: 180,),
+                  FadeAnimation(1.6, AnimatedBuilder(
+                    animation: _scaleController,
+                    builder: (context, child) => Transform.scale(
+                    scale: _scaleAnimation.value,
+                    child: Center(
+                      child: AnimatedBuilder(
+                        animation: _widthController,
+                        builder: (context, child) => Container(
+                          width: _widthAnimation.value,
+                          height: 80,
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50),
+                            color: Colors.blue.withOpacity(.4)
+                          ),
+                          child: InkWell(
+                            onTap: () {
+                              _scaleController.forward();
+                            },
+                            child: Stack(
+                              children: <Widget> [
+                                AnimatedBuilder(
+                                  animation: _positionController,
+                                  builder: (context, child) => Positioned(
+                                    left: _positionAnimation.value,
+                                    child: AnimatedBuilder(
+                                      animation: _scale2Controller,
+                                      builder: (context, child) => Transform.scale(
+                                        scale: _scale2Animation.value,
+                                        child: Container(
+                                          width: 60,
+                                          height: 60,
+                                          decoration: BoxDecoration(
+                                            shape: BoxShape.circle,
+                                            color: Colors.blue
+                                          ),
+                                          child: hideIcon == false ? Icon(Icons.arrow_forward, color: Colors.white,) : Container(),
+                                        )
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ]
                             ),
-                        child: const Text('Get Started')),
-                  ),
+                          ),
+                        ),
+                      ),
+                    )),
+                  )),
+                  SizedBox(height: 60,),
                 ],
               ),
-              const SizedBox(height: 80),
-            ],
-          ),
+            )
+          ],
         ),
       ),
     );
